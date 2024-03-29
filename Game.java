5a340e155f47ff8644a53b5e48551b984a219427
Horacio Mota <horaciomota93@gmail.com>
2024-03-29 17:46:18 +0000

    Adds winner function to the game

--- Game.java
+++ Game.java
@@ -17,7 +17,8 @@ public class Game {
         String computerChoice = computerChoice(randomNumber);
         System.out.println("Computer choice: " + computerChoice );
 
-        System.out.println();
+        String theWinnerIs = decideTheWinner(computerChoice, userChoice);
+        System.out.println(theWinnerIs);
 
     }
         // Criar a escolha da maquina baseado em um numero aletorio criado pela funcao anterior
@@ -26,14 +27,27 @@ public class Game {
                 case 0: return "Rock";
                 case 1: return "Paper";
                 case 2: return "Scissors";
-
                 default: return "Not a valid option";
-            }
-            
+            }     
         }
 
         // Criar aleatoriamente o input da maquina
         public static int generateARandomNumber() {
         return (int) (Math.random() * 3);
         } 
+
+        // Criar as condicoes para decidir o vencedor
+
+        public static String decideTheWinner(String computerChoice, String userChoice ) {
+            if (userChoice.equalsIgnoreCase(computerChoice)) {
+                return "There is no winners today!";                
+            } else if ((userChoice.equalsIgnoreCase("rock") && computerChoice.equalsIgnoreCase("scissors")) ||
+            (userChoice.equalsIgnoreCase("paper") && computerChoice.equalsIgnoreCase("rock")) ||
+            (userChoice.equalsIgnoreCase("scissors") && computerChoice.equalsIgnoreCase("paper"))) {
+     return "You are the winner!";
+            }
+            else {
+                return "You lost, try next time!";
+            }
+            }
 }
