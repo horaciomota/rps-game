5de2b3c1d0847824cc2a4cfa6253d030b49ee2ae
Horacio Mota <horaciomota93@gmail.com>
2024-03-29 17:37:47 +0000

    The logic that adds input for users and compiuters is done

--- Game.java
+++ Game.java
@@ -4,19 +4,35 @@ public class Game {
     public static void main(String[] args) {
         // 1 - Criar o texto que vai pedir o input
         Scanner scan = new Scanner(System.in);
-        System.out.println("Do you wanna play a game? Type: yes or no");
+        int randomNumber = generateARandomNumber();
 
+        System.out.println("Do you wanna play a game? Type: yes or no");
+        String sayYes = scan.nextLine();
+
+        System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.");
+        System.out.print("Shoot!: ");
         String userChoice = scan.nextLine();
         System.out.println("You choose: " + userChoice);
-        String computerChoice = scan.nextLine();
 
-        System.out.println("Computer choice: " + computerChoice);
+        String computerChoice = computerChoice(randomNumber);
+        System.out.println("Computer choice: " + computerChoice );
 
-        // 3 - Comparar e ver quem ganhou 
+        System.out.println();
+
     }
-                
+        // Criar a escolha da maquina baseado em um numero aletorio criado pela funcao anterior
+        public static String computerChoice(int randomNumber) {
+            switch (randomNumber) {
+                case 0: return "Rock";
+                case 1: return "Paper";
+                case 2: return "Scissors";
 
-        //Criar aleatoriamente o input da maquina
+                default: return "Not a valid option";
+            }
+            
+        }
+
+        // Criar aleatoriamente o input da maquina
         public static int generateARandomNumber() {
         return (int) (Math.random() * 3);
         } 
